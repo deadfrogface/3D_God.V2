680635e1c294d440090a5348f1e0f3b3dc2ee075
deadfrogface <dead0frog0face@gmail.com>
2025-06-30 06:33:46 +0200

    Update character_system.py

--- core/character_system/character_system.py
+++ core/character_system/character_system.py
@@ -3,6 +3,8 @@ from .preset_handler import PresetHandler
 from core.rigging.auto_rigger import AutoRigger
 from core.rigging.metahuman_skeleton import MetahumanSkeleton
 from core.rigging.extra_bones import ExtraBoneHandler
+from core.export.fbx_exporter import FBXExporter
+from core.export.preset_saver import PresetSaver
 
 class CharacterSystem:
     def __init__(self):
@@ -11,6 +13,8 @@ class CharacterSystem:
         self.auto_rigger = AutoRigger()
         self.meta_skeleton = MetahumanSkeleton()
         self.extra_bones = ExtraBoneHandler()
+        self.exporter = FBXExporter()
+        self.preset_saver = PresetSaver()
 
         self.current_data = {
             "name": "Unbenannt",
@@ -51,3 +55,8 @@ class CharacterSystem:
     def convert_to_ue5(self):
         return self.meta_skeleton.convert_to_metahuman({})
 
+    def export_to_file(self):
+        return self.exporter.export(self.current_data)
+
+    def save_current_as_preset(self):
+        return self.preset_saver.save(self.current_data)
