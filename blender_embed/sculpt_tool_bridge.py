b4a9aa61e8424b8ebe7d19c5dde464a590eef186
deadfrogface <dead0frog0face@gmail.com>
2025-06-30 04:48:07 +0200

    Update sculpt_tool_bridge.py

--- blender_embed/sculpt_tool_bridge.py
+++ blender_embed/sculpt_tool_bridge.py
@@ -1,1 +1,9 @@
+import subprocess
+import json
+
+def open_blender_sculpt(file_path):
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+        blender_exe = config['blender_path']
+    subprocess.run([blender_exe, file_path])
 
