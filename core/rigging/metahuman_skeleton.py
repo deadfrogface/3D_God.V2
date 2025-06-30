5ab9b5b05ad97a3b77ee4279d87907a41a2aff68
deadfrogface <dead0frog0face@gmail.com>
2025-06-30 05:49:13 +0200

    Create metahuman_skeleton.py

---
+++ core/rigging/metahuman_skeleton.py
@@ -0,0 +1,8 @@
+class MetahumanSkeleton:
+    def __init__(self):
+        print("📐 MetahumanSkeleton geladen")
+
+    def convert_to_metahuman(self, mesh_data):
+        print("🧬 Konvertierung zu UE5 Skeleton läuft...")
+        # Stub – simuliert Konvertierung
+        return True
