26c8fe3207b7ca24f7fbcd63dabdb10a39085cde
deadfrogface <dead0frog0face@gmail.com>
2025-07-01 17:01:19 +0200

    Aktualisieren von material_editor.py

--- ui/panels/material_editor.py
+++ ui/panels/material_editor.py
@@ -1,11 +1,5 @@
-from PySide6.QtWidgets import (
-    QWidget, QVBoxLayout, QLabel, QComboBox, QPushButton, QColorDialog,
-    QSlider, QFileDialog, QHBoxLayout
-)
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QComboBox, QColorDialog
 from core.character_system.character_system import CharacterSystem
-import os
 
 class MaterialEditor(QWidget):
     def __init__(self):
@@ -13,61 +7,30 @@ class MaterialEditor(QWidget):
         self.character_system = CharacterSystem()
         layout = QVBoxLayout()
 
-        layout.addWidget(QLabel("🎨 Material-Editor"))
+        layout.addWidget(QLabel("🎨 Materialeditor"))
 
-        self.material_selector = QComboBox()
-        self.material_selector.addItems(["skin", "clothes", "piercings", "tattoos"])
-        layout.addWidget(QLabel("Material auswählen:"))
-        layout.addWidget(self.material_selector)
+        self.mat_select = QComboBox()
+        self.mat_select.addItems(["skin", "clothes", "piercings", "tattoos"])
+        layout.addWidget(self.mat_select)
 
-        self.color_btn = QPushButton("Farbe wählen")
-        self.color_btn.clicked.connect(self.choose_color)
-        layout.addWidget(self.color_btn)
+        btn_color = QPushButton("Farbe wählen")
+        btn_color.clicked.connect(self.choose_color)
+        layout.addWidget(btn_color)
 
-        layout.addWidget(QLabel("Roughness"))
-        self.rough_slider = QSlider(Qt.Horizontal)
-        self.rough_slider.setRange(0, 100)
-        self.rough_slider.setValue(50)
-        self.rough_slider.valueChanged.connect(self.set_roughness)
-        layout.addWidget(self.rough_slider)
-
-        layout.addWidget(QLabel("Metallic"))
-        self.metal_slider = QSlider(Qt.Horizontal)
-        self.metal_slider.setRange(0, 100)
-        self.metal_slider.setValue(0)
-        self.metal_slider.valueChanged.connect(self.set_metallic)
-        layout.addWidget(self.metal_slider)
-
-        self.texture_preview = QLabel("Keine Textur gewählt")
-        self.texture_preview.setFixedHeight(100)
-        self.texture_preview.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.texture_preview)
-
-        self.texture_btn = QPushButton("Textur auswählen")
-        self.texture_btn.clicked.connect(self.choose_texture)
-        layout.addWidget(self.texture_btn)
+        btn_apply = QPushButton("Auf Preview anwenden")
+        btn_apply.clicked.connect(self.apply_preview_material)
+        layout.addWidget(btn_apply)
 
         self.setLayout(layout)
 
     def choose_color(self):
+        current_mat = self.mat_select.currentText()
+        old_hex = self.character_system.materials.get(current_mat, {}).get("color", "#cccccc")
         color = QColorDialog.getColor()
         if color.isValid():
-            mat = self.material_selector.currentText()
-            self.character_system.set_material_color(mat, color.name())
+            hex_value = color.name()
+            self.character_system.set_material_color(current_mat, hex_value)
 
-    def set_roughness(self, val):
-        mat = self.material_selector.currentText()
-        self.character_system.set_material_value(mat, "roughness", val / 100.0)
-
-    def set_metallic(self, val):
-        mat = self.material_selector.currentText()
-        self.character_system.set_material_value(mat, "metallic", val / 100.0)
-
-    def choose_texture(self):
-        path, _ = QFileDialog.getOpenFileName(self, "Textur auswählen", "", "Bilder (*.png *.jpg *.jpeg)")
-        if path:
-            mat = self.material_selector.currentText()
-            self.character_system.set_material_texture(mat, path)
-            pix = QPixmap(path).scaledToHeight(100, Qt.SmoothTransformation)
-            self.texture_preview.setPixmap(pix)
-            self.texture_preview.setText("")
+    def apply_preview_material(self):
+        if self.character_system.viewport_ref:
+            self.character_system.viewport_ref.show_materials(self.character_system.materials)
