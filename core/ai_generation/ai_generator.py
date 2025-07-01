4057120e5154a2eeef88736aaaf6c82a8696a5f8
deadfrogface <dead0frog0face@gmail.com>
2025-07-01 13:51:13 +0200

    Aktualisieren von ai_generator.py

--- core/ai_generation/ai_generator.py
+++ core/ai_generation/ai_generator.py
@@ -1,10 +1,14 @@
 from core.ai_generation.fauxpilot_handler import FauxPilotHandler
 from core.ai_generation.triposr_handler import TripoSRHandler
+from core.ai_generation.charmorph_handler import CharMorphHandler
+from core.character_system.character_system import CharacterSystem
 
 class AIGenerator:
     def __init__(self):
         self.fauxpilot = FauxPilotHandler()
         self.triposr = TripoSRHandler()
+        self.charmorph = CharMorphHandler()
+        self.character_system = CharacterSystem()
 
     def generate_code(self, prompt):
         return self.fauxpilot.generate(prompt)
@@ -14,3 +18,6 @@ class AIGenerator:
 
     def generate_mesh_from_image(self):
         self.triposr.generate_mesh()
+
+    def generate_shape_from_prompt(self, prompt):
+        return self.charmorph.generate_shape(prompt)
