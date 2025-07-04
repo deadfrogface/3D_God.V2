278eb32ca320234295cd5ba0f739fff5b2f6ba6d
deadfrogface <dead0frog0face@gmail.com>
2025-07-04 20:42:54 +0200

    Aktualisieren von autorig_basic.py

--- blender_embed/scripts/autorig_basic.py
+++ blender_embed/scripts/autorig_basic.py
@@ -1,2 +1,4 @@
-print("[Rig][AutoRig] ▶️ Starte einfaches Auto-Rigging...")
+from core.logger import log
+
+log.info("[Rig][AutoRig] ▶️ Starte einfaches Auto-Rigging...")
 # → Hier wird z.B. Rigify verwendet
