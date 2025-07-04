2ef19f3642642ce0812730922554c58de0bc354a
deadfrogface <dead0frog0face@gmail.com>
2025-07-04 15:45:18 +0200

    Aktualisieren von logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,23 +1,21 @@
+# core/logger.py
+
 import logging
 
 def setup_logger(name="3DGod", level=logging.DEBUG, logfile="debug_log.txt"):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    # Formatter mit Zeitstempel und Modul-Info
     formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s] %(message)s', datefmt='%H:%M:%S')
 
-    # Konsole
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
-    # Datei
-    file_handler = logging.FileHandler(logfile, encoding='utf-8')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    fh = logging.FileHandler(logfile, encoding='utf-8')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
     return logger
 
-# Globale Instanz
 log = setup_logger()
