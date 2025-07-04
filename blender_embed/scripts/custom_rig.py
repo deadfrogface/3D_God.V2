c45912ec0cb3c6b96fbb3c4806375159342ce6e6
deadfrogface <dead0frog0face@gmail.com>
2025-07-04 20:41:56 +0200

    Aktualisieren von custom_rig.py

--- blender_embed/scripts/custom_rig.py
+++ blender_embed/scripts/custom_rig.py
@@ -1,5 +1,6 @@
 from core.logger import log
 
-log.info("[Rig][CustomRig] ▶️ Benutzerdefiniertes Rigging-Skript gestartet...")
+log.info("[Rig][AutoRig] ▶️ Starte einfaches Auto-Rigging...")
+# → Hier wird z.B. Rigify verwendet
 # → Nutze deine eigenen Bone-Namen oder Constraints
 # (Diese Zeile bleibt als Hinweis für spätere Entwickler erhalten)
